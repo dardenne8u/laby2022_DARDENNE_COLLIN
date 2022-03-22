2fc90f63798ab1c56fad75779d584062b03f7a41
Gregory DARDENNE <gdardenne0@gmail.com>
2022-03-22 15:13:07 +0100

    ajout de la methode static getSuivant

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -15,7 +15,28 @@ class Labyrinthe{
 
 
     static int[] getSuivant(int x, int y, String action) {
-        throw new Error("TODO");
+        int[] resPostion = new int[2];
+        switch (action){
+            case "HAUT":
+                resPostion[0] = x-1;
+                resPostion[1] = y;
+                break;
+            case "BAS":
+                resPostion[0] = x+1;
+                resPostion[1] = y;
+                break;
+            case "GAUCHE":
+                resPostion[0] = x;
+                resPostion[1] = y-1;
+                break;
+            case "DROITE":
+                resPostion[0] = x;
+                resPostion[1] = y+1;
+                break;
+            default:
+                throw new ActionInconnueException("Direction inconnue !");
+        }
+        return  resPostion;
     }
 
 
