f766e2b51bdd26cae5c3b1dac22055c99880fdab
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-03-22 15:45:11 +0100

    Update Sortie.java

--- src/Sortie.java
+++ src/Sortie.java
@@ -1,10 +1,18 @@
-public class Sortie
+public class Sortie extends Position
 {
     //Attributes
     private int x, y;
 
     //Constructors
-    public Sortie
+    public Sortie ()
+    {
+        super(0,0);
+    }
+
+    public Sortie (int xp, int yp)
+    {
+        super(xp,yp);
+    }
 
     //Methods
 }
