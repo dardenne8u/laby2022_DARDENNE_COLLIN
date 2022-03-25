c9a2e29afc242d3a27993a0ec318d6644e4c5cfb
Gregory DARDENNE <gdardenne0@gmail.com>
2022-03-25 11:41:29 +0100

    rectification main laby (ajout try/catch

--- src/MainLaby.java
+++ src/MainLaby.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainLaby
@@ -6,7 +7,17 @@ public class MainLaby
     public static void main(String[] args)
     {
         String commande= "";
-        Labyrinthe laby = Labyrinthe.chargerLabyrinthe(args[0]);
+        Labyrinthe laby;
+        try {
+            laby = Labyrinthe.chargerLabyrinthe(args[0]);
+        } catch (FileNotFoundException e){
+            System.out.println("Nom du fichier inexistant");
+        } catch (IOException e){
+            System.out.println("Probleme a la lecture du fichier");
+        } catch (FichierIncorrectException e){
+            System.out.println(e);
+        }
+
         while (commande != "exit")
         {
             System.out.println(laby);
