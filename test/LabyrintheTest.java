c01a111952c8d12d17b8fe0b568ed74f7c0797cf
Gregory DARDENNE <gdardenne0@gmail.com>
2022-03-25 12:00:59 +0100

    ajout du fichier de test laby

---
+++ test/LabyrintheTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabyrintheTest {
+
+    @Test
+    public void test_charger() throws FichierIncorrectException, IOException {
+        // Initialisation
+        Labyrinthe atester = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
+
+        //Labyrinthe
+
+    }
+
+}
