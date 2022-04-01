55b6c60934d20d1510f21340477e8cf256fa203a
DARDENNE Gregory <gdardenne0@gmail.com>
2022-04-01 14:11:57 +0200

    Ajout du test de charger

--- test/LabyrintheTest.java
+++ test/LabyrintheTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LabyrintheTest {
 
@@ -11,8 +11,18 @@ class LabyrintheTest {
         // Initialisation
         Labyrinthe atester = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
 
-        //Labyrinthe
+        // Labyrinthe qu'on doit obtenir
+        boolean[][] murs = new boolean[5][7];
+        murs[0] = new boolean[] {true,true,true,true,true,true,true};
+        murs[1] = new boolean[] {true,false,false,false,false,false,true};
+        murs[2] = new boolean[] {true,false,false,false,false,false,true};
+        murs[3] = new boolean[] {true,false,false,false,false,false,true};
+        murs[4] = new boolean[] {true,true,true,true,true,true,true};
 
+        Labyrinthe resultat = new Labyrinthe(new Personnage(2,3), new Sortie(1,1),murs);
+
+        // Test du labyrinthe
+        assertEquals(resultat, atester, "Mauvais labyrinthe");
     }
 
 }
