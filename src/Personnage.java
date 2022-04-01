cf3d631f0aae3f62998b48f267a70bca367fa28f
DARDENNE Gregory <gdardenne0@gmail.com>
2022-04-01 13:47:26 +0200

    add equals method

--- src/Personnage.java
+++ src/Personnage.java
@@ -3,4 +3,10 @@ public class Personnage extends Position{
     public Personnage(int x, int y){
         super(x, y);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        Personnage p = (Personnage) obj;
+        return this.getX() == p.getX() && this.getY() == p.getY();
+    }
 }
