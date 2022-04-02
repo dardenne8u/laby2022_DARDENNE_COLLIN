31d0d31c09a7296cf910d8d7852a9f390bde4273
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-04-02 16:27:06 +0200

    ajout test_toString_ok

--- test/LabyrintheTest.java
+++ test/LabyrintheTest.java
@@ -172,7 +172,46 @@ class LabyrintheTest {
     }
 
 
-    //getSuivant, deplacerPerso, toString
+    //TODO : getSuivant, deplacerPerso
+
+
+    /**
+     * Teste si la fonction toString renvoie la chaine attendue
+     */
+    @Test
+    public void test_toString_ok ()
+    {
+        //Preparation des donnees
+        murs = new boolean[5][7];
+        murs[0] = new boolean[] {true,true,true,true,true,true,true};
+        murs[1] = new boolean[] {true,false,false,false,false,false,true};
+        murs[2] = new boolean[] {true,false,false,false,false,false,true};
+        murs[3] = new boolean[] {true,false,false,false,false,false,true};
+        murs[4] = new boolean[] {true,true,true,true,true,true,true};
+
+        p = new Personnage(2,3);
+        s = new Sortie(1,1);
+
+        laby = new Labyrinthe(p, s, murs);
+
+        String expected = """
+                Etat du labyrinthe :
+                Dimensions : \t5:7
+                Personnage : \t2:3
+                Sortie :\t\t1:1
+                XXXXXXX
+                XS....X
+                X..P..X
+                X.....X
+                XXXXXXX
+                """;
+
+        //Appel de la methode
+        boolean res = laby.toString().equals(expected);
+
+        //Test
+        assertTrue(res, "doit etre la meme chaine");
+    }
 
 
     /**
@@ -279,10 +318,13 @@ class LabyrintheTest {
         murs[3] = new boolean[] {true,false,false,false,false,false,true};
         murs[4] = new boolean[] {true,true,true,true,true,true,true};
 
-        Labyrinthe resultat = new Labyrinthe(new Personnage(2,3), new Sortie(1,1),murs);
+        p = new Personnage(2,3);
+        s = new Sortie(1,1);
+
+        laby = new Labyrinthe(p, s, murs);
 
         // Test du labyrinthe
-        assertEquals(resultat, atester, "Mauvais labyrinthe");
+        assertEquals(laby, atester, "Mauvais labyrinthe");
     }
 
 
